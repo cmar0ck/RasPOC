d70029eb65529989cd6a0a9b51492f693d7914c3
cmar0ck <cmar0ck@users.noreply.github.com>
2017-07-22 12:01:24 +0200

    Update scan.sh

--- raspoc/scan.sh
+++ raspoc/scan.sh
@@ -3,7 +3,7 @@
 # CONFIGURATION
 ENV="~/raspoc" 	# Working directory (for e.g '~/raspoc')
 FRQ="" 		# Frequency you want to scan (for e.g. 123.000M") 
-PPM="" 		# Inaccuracy correction offset of your SDR device (can be figured out with gqrx)
+PPM="0" 		# Inaccuracy correction offset of your SDR device (can be figured out with gqrx)
 
 # START SCANNING 
 rtl_fm -f $FRQ -p $PPM -s 22050 | multimon-ng -t raw -a POCSAG512 -a POCSAG1200 -a POCSAG2400 -f alpha /dev/stdin | while IFS= read -r line; do echo "$(date) $line"; done >> $ENV/scan.log
